486f46f0a938b2d24a88e331e944399ec3189eb9
Hailey_ys <whassup_man@naver.com>
2022-12-19 11:29:46 +0900

    Setting Folder

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
+
+import Login from './pages/Login/Login';
+import Main from './pages/Main/Main';
+import Signup from './pages/Signup/Signup';
+
 const Router = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
-        <Route path='/' element={<Components /> />
+        <Route path='/' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/Main' element={<Main />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
-    )
-}
+  );
+};
 
-export default Router
+export default Router;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,8 @@
-function Login() {
-  return 'hi';
-}
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  return <Link to='/signup'>회원가입</Link>;
+};
 
 export default Login;
